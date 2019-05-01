c0273f2a6c4d61d52caebc243be746f2c90e2190
annapalouzie <49191475+annapalouzie@users.noreply.github.com>
2019-05-01 11:28:36 +0200

    Pause
    He continuat l'escena de pausa

---
+++ JOOC/Menú pausa/Control.gd
@@ -0,0 +1,26 @@
+extends Control
+
+func _on_Pause_pressed():
+	if get_tree().paused == true:
+		get_tree().paused = false
+		$Tween.interpolate_property($Buttons,'rect_position',$Buttons.rect_position,$Buttons.rect_position-Vector2(800,0),1,Tween.TRANS_BACK,Tween.EASE_IN)
+		$Tween.start()
+	else:
+		get_tree().paused = true
+		$Tween.interpolate_property($Buttons,'rect_position',$Buttons.rect_position,$Buttons.rect_position+Vector2(800,0),1,Tween.TRANS_BACK,Tween.EASE_IN)
+		$Tween.start()
+	
+func _on_Continue_pressed():
+	if get_tree().paused == true:
+		get_tree().paused = false
+		$Tween.interpolate_property($Buttons,'rect_position',$Buttons.rect_position,$Buttons.rect_position-Vector2(800,0),1,Tween.TRANS_BACK,Tween.EASE_IN)
+		$Tween.start()
+
+func _on_Return_pressed():
+	get_tree().change_scene("res://Menú principal/Menú.tscn")
+
+func _on_Exit_pressed():
+	get_tree().quit()
+
+
+
--- JOOC/Menú pausa/Pause.gd
+++ JOOC/Menú pausa/Pause.gd
@@ -1,8 +1,1 @@
 extends TextureButton
-
-
-func _on_Pause_pressed():
-	if get_tree().paused == true:
-		get_tree().paused = false
-	else:
-		get_tree().paused = true
