d53cbd465b2ade63225ad3a9467f97ac928f00b5
Tinsooon <49191404+Tinsooon@users.noreply.github.com>
2019-05-08 11:57:37 +0200

    script moneda
     inetnto

--- JOOC/ESCENES/pastanagues/escena pastanagues.gd
+++ JOOC/ESCENES/pastanagues/escena pastanagues.gd
@@ -1,11 +1,12 @@
 extends Area2D
 
+var pastanaga = load("res://ESCENES/pastanagues/escena pastanagues.tscn")
+
 func ready(): 
 	pass
 
 
-
-func _on_moneda_body_entered(area):
-	if area.get_name() == "conill":
+func _on_Pastanaga_body_entered(body):
+	if body.get_name() == "conill":
 		self.queue_free()
-		#global_var.Pastanaga += 20
+		
